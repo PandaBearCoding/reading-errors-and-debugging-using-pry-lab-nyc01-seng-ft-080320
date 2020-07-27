c3a1a3f821286758908fb086ecd680233d3d49eb
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-27 20:51:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -14,12 +14,10 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
-    binding.pry
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-  binding.pry
-     "HAM HAM HAM IN MY TUMMY"
+  if num == 1
+     puts "YUM YUM MUNCH MUNCH MUNCH"
+  elsif num == 2
+     puts "HAM HAM HAM IN MY TUMMY"
   end
 end
 
