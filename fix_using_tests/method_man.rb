d18940ac5cb7c671e99167a664b429b7e4d4fe0e
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-27 21:28:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -13,3 +13,12 @@ def play_game(player1, player2)
 end
 
 
+def start_game(player1, player2)
+  puts "Hello #{player1} & #{player2}"
+  #binding.pry
+end
+
+def play_game(player1, player2)
+  puts "#{player1} is better than #{player2}"
+  #binding.pry
+end
