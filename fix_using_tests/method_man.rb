2d34f1277f3d9eac3de52d611ea3c7823e132c14
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-27 21:41:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,10 +2,10 @@
 
 require 'pry'
 
-def start_game("Rocky", "Bullwinkle")
-  puts "Hello #{Rocky} & #{Bullwinkle}"
+def start_game(player1 == "Rocky", player2 == "Bullwinkle")
+  puts "Hello #{player1} & #{player2}"
 end
 
-def play_game("Jay-Z", "Beyonce")
-  puts "#{Jay-Z} is better than #{Beyonce}"
+def play_game(player1 == "Jay-Z", player2 == "Beyonce")
+  puts "#{player1} is better than #{player2}"
 end 
