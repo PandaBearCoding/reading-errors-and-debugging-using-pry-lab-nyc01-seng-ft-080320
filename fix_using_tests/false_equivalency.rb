84125b014c2634b90750ce6be4c5560c5f3c7353
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-27 21:04:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,11 +13,12 @@ def prompt_user
   puts "2.) Eat a ham."
 end
 
+num = 1
 def selection(num)
-  if num == 1
+  if num = 1
     #binding.pry
      puts "YUM YUM MUNCH MUNCH MUNCH"
-  else num == 2
+  else num = 2
     #binding.pry
      puts "HAM HAM HAM IN MY TUMMY"
   end
