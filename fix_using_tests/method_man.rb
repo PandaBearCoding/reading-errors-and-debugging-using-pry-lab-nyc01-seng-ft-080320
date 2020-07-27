38e94c6ce28e30b7226b1cff9e2054ca9331314f
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-27 21:24:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,11 +3,15 @@
 require 'pry'
 
 def start_game(player1, player2)
+  player1 == "Rocky"
+  player2 == "Bullwinkle"
   puts "Hello #{player1} & #{player2}"
   #binding.pry
 end
 
 def play_game(player1, player2)
+  player1 == "Rocky"
+  player2 == "Bullwinkle"
   puts "#{player_1} is better than #{player2}"
   #binding.pry
 end
